9e4d8b2b2bcdb49a1c4405fc9e7cee89c3acc21d
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-14 22:15:40 +0100

    Adding Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.ico';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
 
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
